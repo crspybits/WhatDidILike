c135546a7b456fdae42b67a09ffe3d5a19ca30ad
Christopher Prince <chris@SpasticMuffin.biz>
2017-08-22 22:18:02 -0600

    Working on converting old data format to new.

---
+++ Code/Core Data/ConvertFromV1.swift
@@ -0,0 +1,9 @@
+//
+//  ConvertFromV1.swift
+//  WhatDidILike
+//
+//  Created by Christopher G Prince on 8/21/17.
+//  Copyright © 2017 Spastic Muffin, LLC. All rights reserved.
+//
+
+import Foundation
---
+++ Code/Core Data/Place+CoreDataProperties.swift
@@ -0,0 +1,61 @@
+//
+//  Place+CoreDataProperties.swift
+//  WhatDidILike
+//
+//  Created by Christopher G Prince on 8/21/17.
+//  Copyright © 2017 Spastic Muffin, LLC. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Place {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Place> {
+        return NSFetchRequest<Place>(entityName: "Place")
+    }
+
+    @NSManaged public var generalDescription: String?
+    @NSManaged public var name: String?
+    @NSManaged public var category: String?
+    @NSManaged public var csvListNames: String?
+    @NSManaged public var items: NSSet?
+    @NSManaged public var locations: NSSet?
+
+}
+
+// MARK: Generated accessors for items
+extension Place {
+
+    @objc(addItemsObject:)
+    @NSManaged public func addToItems(_ value: Item)
+
+    @objc(removeItemsObject:)
+    @NSManaged public func removeFromItems(_ value: Item)
+
+    @objc(addItems:)
+    @NSManaged public func addToItems(_ values: NSSet)
+
+    @objc(removeItems:)
+    @NSManaged public func removeFromItems(_ values: NSSet)
+
+}
+
+// MARK: Generated accessors for locations
+extension Place {
+
+    @objc(addLocationsObject:)
+    @NSManaged public func addToLocations(_ value: Location)
+
+    @objc(removeLocationsObject:)
+    @NSManaged public func removeFromLocations(_ value: Location)
+
+    @objc(addLocations:)
+    @NSManaged public func addToLocations(_ values: NSSet)
+
+    @objc(removeLocations:)
+    @NSManaged public func removeFromLocations(_ values: NSSet)
+
+}
